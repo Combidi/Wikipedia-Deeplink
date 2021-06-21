1b289f3fef316d4f1ede9cc67d6ff73bda2f3859
Peter Combee <petercombee@gmail.com>
2021-06-21 11:53:29 +0200

    Added preview for LocationsViewController

--- Wikipedia Deeplink/View/LocationsViewController.swift
+++ Wikipedia Deeplink/View/LocationsViewController.swift
@@ -6,4 +6,17 @@ import UIKit
 
 class LocationsViewController: UIViewController {
         
+    override func viewDidLoad() {
+        view.backgroundColor = .yellow
+    }
 }
+
+#if DEBUG
+import SwiftUI
+
+struct LocationsViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable(LocationsViewController())
+    }
+}
+#endif
