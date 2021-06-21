762c003d163cea223c32ae2e33a57fcb9558def0
Peter Combee <petercombee@gmail.com>
2021-06-21 11:18:47 +0200

    Initial Commit

---
+++ Wikipedia Deeplink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wikipedia Deeplink
+//
+//  Created by Peter Combee on 21/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
