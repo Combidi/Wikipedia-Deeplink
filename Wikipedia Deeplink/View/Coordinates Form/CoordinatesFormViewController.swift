a42dc5e10e4547fb46b02376d685a7664dbe2be9
Peter Combee <petercombee@gmail.com>
2021-06-21 19:55:03 +0200

    Touch CoordinatesFormViewController

---
+++ Wikipedia Deeplink/View/Coordinates Form/CoordinatesFormViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Peter Combee on 21/06/2021.
+//
+
+import UIKit
+
+class CoordinatesFormViewController: UIViewController {
+    
+}
---
+++ Wikipedia DeeplinkTests/View/Coordinates Form/CoordinatesFormViewControllerTest.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Peter Combee on 21/06/2021.
+//
+
+import XCTest
+@testable import Wikipedia_Deeplink
+
+class CoordinatesFormViewControllerTest: XCTestCase {
+    
+    func test() {
+        let _ = CoordinatesFormViewController()
+    }
+}
