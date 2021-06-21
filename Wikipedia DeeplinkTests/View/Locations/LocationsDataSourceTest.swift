f7c9d579930d2ff1bf5f3651e2b74086d929ca1c
Peter Combee <petercombee@gmail.com>
2021-06-21 15:27:51 +0200

    Refactoring LocationsDataSourceTest

--- Wikipedia DeeplinkTests/View/Locations/LocationsDataSourceTest.swift
+++ Wikipedia DeeplinkTests/View/Locations/LocationsDataSourceTest.swift
@@ -7,21 +7,21 @@ import XCTest
 
 class LocationsDataSourceTest: XCTestCase {
     
+    private var sut: LocationsDataSource!
+    private let locations: [Location] = [
+        .init(name: "Arnhem"),
+        .init(name: "Velp")
+    ]
+    
+    override func setUp() {
+        sut = .init(locations)
+    }
+    
     func test_numberOfRows() {
-        let locations = [
-            Location(name: "Arnhem"),
-            Location(name: "Velp")
-        ]
-        let sut = LocationsDataSource(locations)
         XCTAssertEqual(sut.tableView(.init(), numberOfRowsInSection: 0), locations.count)
     }
         
     func test_cell() {
-        let locations = [
-            Location(name: "Arnhem"),
-            Location(name: "Velp")
-        ]
-        let sut = LocationsDataSource(locations)
         let cell = sut.tableView(.init(), cellForRowAt: .init(row: 1, section: 0))
         XCTAssertEqual(cell.textLabel?.text, locations[1].name)
     }
