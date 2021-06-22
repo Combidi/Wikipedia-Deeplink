8b6aa5737304aaa0c28bb33ee73120532bd12b08
Peter Combee <petercombee@gmail.com>
2021-06-22 09:34:39 +0200

    Touch NavigationController

---
+++ Wikipedia Deeplink/View/NavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  Created by Peter Combee on 22/06/2021.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    
+}
+
+#if DEBUG
+import SwiftUI
+
+struct NavigationController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable(NavigationController())
+    }
+}
+#endif
---
+++ Wikipedia DeeplinkTests/View/NavigationControllerTest.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Peter Combee on 22/06/2021.
+//
+
+import XCTest
+@testable import Wikipedia_Deeplink
+
+class NavigationControllerTest: XCTestCase {
+    
+    func test() {
+        let sut = NavigationController()
+    }
+}
